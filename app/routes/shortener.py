bf0064ff9dbf0b7d79891c7336f9e96453d70bec
Nelson Estevão <nelsonmestevao@gmail.com>
2020-06-23 17:07:18 +0100

    Create url shortener api (#1)

---
+++ app/routes/shortener.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, request, abort
+
+from app.extensions.database import db
+from app.models.link import Link
+
+short = Blueprint('short', __name__)
+
+@short.route('/u/<slug>', methods=['GET'])
+def get_url(slug):
+    """GET the url from a slug"""
+    link = Link.query.filter_by(slug=slug).first_or_404()
+
+    link.visits = link.visits + 1
+    db.session.commit()
+
+    return {"url": link.url}
+
+
+@short.route('/u', methods=['POST'])
+def create_slug():
+    """POST a new slug for a url"""
+    if not request.json or not 'url' in request.json:
+        abort(400)
+
+    link = Link(url=request.json['url'])
+
+    db.session.add(link)
+    db.session.commit()
+
+    return {"link": "https://estev.ao/" + link.slug}
